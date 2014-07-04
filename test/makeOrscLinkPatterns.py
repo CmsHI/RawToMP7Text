f1a467f322e0e7e38f3b31a8451ace7033ca16c8
R. Alex Barbieri <luck@mit.edu>
2014-07-04 15:29:35 +0200

    move python to consumes

--- test/makeOrscLinkPatterns.py
+++ test/makeOrscLinkPatterns.py
@@ -12,7 +12,7 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing('analysis')
 
 options.inputFiles = (
-    'file:/hdfs/store/user/tapas/2012-08-01-CRAB_ZEESkim/skim_10_1_wd2.root')
+    'file:/afs/cern.ch/work/g/ginnocen/public/skim_10_1_wd2.root')
 options.maxEvents = 2
 options.parseArguments()
 
@@ -34,12 +34,12 @@ process.load("Configuration.Geometry.GeometryIdeal_cff")
 process.load("L1Trigger.L1TCalorimeter.L1TCaloStage1_PPFromRaw_cff")
 
 process.patterns = cms.EDAnalyzer('OrscLinkPatterns',
-                                  src = cms.InputTag("simRctDigis")
+                                  src = cms.InputTag("simRctDigis"))
 
 process.pattern_sequence = cms.Sequence(
     process.L1TRerunHCALTP_FromRAW
     +process.ecalDigis
     +process.simRctDigis
-    process.patterns)
+    +process.patterns)
 
 process.p = cms.Path(process.pattern_sequence)
