e48dd404c4dc15769fb27e5a6d27594419176e49
gian michele innocenti <ginnocen@cern.ch>
2014-07-12 00:15:13 +0200

    new database format class and new converter from 32bit words to physics quantities

---
+++ interface/PhysicsToBitConverter.h
@@ -0,0 +1,52 @@
+#ifndef _PHYSICSTOBITCONVERTER_h
+#define _PHYSICSTOBITCONVERTER_h
+
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <stdint.h>
+#include <iomanip>  
+#include <sstream>    
+#include <vector>
+#include <array>
+#include <bitset>
+#include "DataBase.h"
+
+class PhysicsToBitConverter {
+    
+                    
+  int words32bitLink[2][6];  //[link][word]
+    
+  std::vector<int> bitsLink[2];
+  DataBase databaseobject;
+
+  
+  public:
+  
+    PhysicsToBitConverter();
+    ~PhysicsToBitConverter() { }
+    void Set32bitWordLink0(int index,int value){words32bitLink[0][index]=value;};
+    void Set32bitWordLink1(int index,int value){words32bitLink[1][index]=value;};
+    void Convert();
+    
+    int GetRCEt(int card,int region);
+    int GetHFEt(int region);
+    int GetRCTau(int card,int region);
+    int GetRCOf(int card,int region);
+    int GetHFFg(int region);
+    int GetNEPos(int cand);
+    int GetNEEt(int cand);
+    int GetIEPos(int cand);
+    int GetIEEt(int cand);
+    int GetRCHad(int card,int region);
+
+    
+    int ReadBitInInt(int bit,int value);
+    int BuildPhysicsValue(int firstbit,int bitlength,int linkid);
+    
+};
+
+#endif
---
+++ src/PhysicsToBitConverter.cc
@@ -0,0 +1,126 @@
+#include "PhysicsToBitConverter.h"
+
+PhysicsToBitConverter::PhysicsToBitConverter() {
+
+}
+
+void PhysicsToBitConverter::Convert() {
+
+  for (int iword=0;iword<6;iword++){
+    for (int ibit=0;ibit<32;ibit++){
+      bitsLink[0].push_back(ReadBitInInt(ibit,words32bitLink[0][iword]));
+      bitsLink[1].push_back(ReadBitInInt(ibit,words32bitLink[1][iword]));
+    }
+  }
+}
+
+int PhysicsToBitConverter::GetRCEt(int card,int region){
+
+  int mystart=databaseobject.GetIndicesRCEt(card,region);
+  int mylength=databaseobject.GetRCEtLength();
+  int mylink=databaseobject.GetRCEtLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetHFEt(int region){
+
+  int mystart=databaseobject.GetIndicesHFEt(region);
+  int mylength=databaseobject.GetHFEtLength();
+  int mylink=databaseobject.GetHFEtLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetRCTau(int card,int region){
+
+  int mystart=databaseobject.GetIndicesRCTau(card,region);
+  int mylength=databaseobject.GetRCTauLength();
+  int mylink=databaseobject.GetRCTauLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetRCOf(int card,int region){
+
+  int mystart=databaseobject.GetIndicesRCOf(card,region);
+  int mylength=databaseobject.GetRCOfLength();
+  int mylink=databaseobject.GetRCOfLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetHFFg(int region){
+
+  int mystart=databaseobject.GetIndicesHFFg(region);
+  int mylength=databaseobject.GetHFFgLength();
+  int mylink=databaseobject.GetHFFgLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetNEPos(int cand){
+
+  int mystart=databaseobject.GetIndicesNEPos(cand);
+  int mylength=databaseobject.GetNEPosLength();
+  int mylink=databaseobject.GetNEPosLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetNEEt(int cand){
+
+  int mystart=databaseobject.GetIndicesNEEt(cand);
+  std::cout<<"GetIndicesNEEt"<<mystart<<std::endl;
+  int mylength=databaseobject.GetNEEtLength();
+  int mylink=databaseobject.GetNEEtLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetIEPos(int cand){
+
+  int mystart=databaseobject.GetIndicesIEPos(cand);
+  int mylength=databaseobject.GetIEPosLength();
+  int mylink=databaseobject.GetIEPosLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetIEEt(int cand){
+
+  int mystart=databaseobject.GetIndicesIEEt(cand);
+  int mylength=databaseobject.GetIEEtLength();
+  int mylink=databaseobject.GetIEEtLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+int PhysicsToBitConverter::GetRCHad(int card,int region){
+
+  int mystart=databaseobject.GetIndicesRCHad(card,region);
+  int mylength=databaseobject.GetRCHadLength();
+  int mylink=databaseobject.GetRCHadLink();
+  
+  return BuildPhysicsValue(mystart,mylength,mylink);
+}
+
+
+int PhysicsToBitConverter::ReadBitInInt(int bit,int value){
+
+   std::bitset<32> foo(value);
+   return foo[bit];
+  
+}
+
+int PhysicsToBitConverter::BuildPhysicsValue(int firstbit,int bitlength,int linkid){
+  
+  int myvalue=0;
+  int counter=0;
+  
+  for (int m=firstbit;m<firstbit+bitlength;m++){
+    myvalue|=(bitsLink[linkid][m]&(0x1)) << counter;
+    counter++;
+  }
+  return myvalue;
+}
