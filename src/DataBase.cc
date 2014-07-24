a8f94b409fc6e77b12d210d3ed811c88f778a235
R. Alex Barbieri <luck@mit.edu>
2014-07-24 20:47:17 +0200

    more build fixes, accidental whitespace cleanup

--- src/DataBase.cc
+++ src/DataBase.cc
@@ -1,4 +1,4 @@
-#include "DataBase.h"
+#include "../interface/DataBase.h"
 
 
 DataBase::DataBase() {
@@ -22,106 +22,105 @@ DataBase::DataBase() {
   IEEtlink=1;
   IEPoslink=1;
   NEEtlink=1;
-  NEPoslink=1; 
+  NEPoslink=1;
   RCHadlink=1;
 
-  RCEt_start[0][0]=8;  
-  RCEt_start[0][1]=18;  
-  RCEt_start[1][0]=28;  
-  RCEt_start[1][1]=38;  
-  RCEt_start[2][0]=48;  
-  RCEt_start[2][1]=58;  
-  RCEt_start[3][0]=68; 
-  RCEt_start[3][1]=78; 
-  RCEt_start[4][0]=88;  
-  RCEt_start[4][1]=98;  
-  RCEt_start[5][0]=108; 
-  RCEt_start[5][1]=118; 
-  RCEt_start[6][0]=128; 
+  RCEt_start[0][0]=8;
+  RCEt_start[0][1]=18;
+  RCEt_start[1][0]=28;
+  RCEt_start[1][1]=38;
+  RCEt_start[2][0]=48;
+  RCEt_start[2][1]=58;
+  RCEt_start[3][0]=68;
+  RCEt_start[3][1]=78;
+  RCEt_start[4][0]=88;
+  RCEt_start[4][1]=98;
+  RCEt_start[5][0]=108;
+  RCEt_start[5][1]=118;
+  RCEt_start[6][0]=128;
   RCEt_start[6][1]=138;
-  
-  RCTau_start[0][0]=148; 
-  RCTau_start[0][1]=149; 
-  RCTau_start[1][0]=150; 
-  RCTau_start[1][1]=151; 
-  RCTau_start[2][0]=152; 
-  RCTau_start[2][1]=153; 
+
+  RCTau_start[0][0]=148;
+  RCTau_start[0][1]=149;
+  RCTau_start[1][0]=150;
+  RCTau_start[1][1]=151;
+  RCTau_start[2][0]=152;
+  RCTau_start[2][1]=153;
   RCTau_start[3][0]=154;
-  RCTau_start[3][1]=155; 
-  RCTau_start[4][0]=156; 
-  RCTau_start[4][1]=157; 
-  RCTau_start[5][0]=158; 
-  RCTau_start[5][1]=159; 
-  RCTau_start[6][0]=160; 
+  RCTau_start[3][1]=155;
+  RCTau_start[4][0]=156;
+  RCTau_start[4][1]=157;
+  RCTau_start[5][0]=158;
+  RCTau_start[5][1]=159;
+  RCTau_start[6][0]=160;
   RCTau_start[6][1]=161;
-  
-  RCOf_start[0][0]=162; 
-  RCOf_start[0][1]=163; 
-  RCOf_start[1][0]=164; 
-  RCOf_start[1][1]=165; 
-  RCOf_start[2][0]=166; 
-  RCOf_start[2][1]=167; 
+
+  RCOf_start[0][0]=162;
+  RCOf_start[0][1]=163;
+  RCOf_start[1][0]=164;
+  RCOf_start[1][1]=165;
+  RCOf_start[2][0]=166;
+  RCOf_start[2][1]=167;
   RCOf_start[3][0]=168;
-  RCOf_start[3][1]=169; 
-  RCOf_start[4][0]=170; 
-  RCOf_start[4][1]=171; 
-  RCOf_start[5][0]=172; 
-  RCOf_start[5][1]=173; 
-  RCOf_start[6][0]=174; 
+  RCOf_start[3][1]=169;
+  RCOf_start[4][0]=170;
+  RCOf_start[4][1]=171;
+  RCOf_start[5][0]=172;
+  RCOf_start[5][1]=173;
+  RCOf_start[6][0]=174;
   RCOf_start[6][1]=175;
-  
-  HFEt_start[0]=8; 
-  HFEt_start[1]=16; 
-  HFEt_start[2]=24; 
-  HFEt_start[3]=32; 
-  HFEt_start[4]=40; 
-  HFEt_start[5]=48; 
-  HFEt_start[6]=56; 
-  HFEt_start[7]=64; 
-  
-  HFFg_start[0]=72; 
-  HFFg_start[1]=73; 
-  HFFg_start[2]=74; 
-  HFFg_start[3]=75; 
-  HFFg_start[4]=76; 
-  HFFg_start[5]=77; 
-  HFFg_start[6]=78; 
-  HFFg_start[7]=79; 
-  
-  IEEt_start[0]=80; 
-  IEEt_start[1]=90; 
-  IEEt_start[2]=100; 
-  IEEt_start[3]=110; 
-  
-  IEPos_start[0]=86; 
-  IEPos_start[1]=96; 
-  IEPos_start[2]=106; 
-  IEPos_start[3]=116; 
-  
-  NEEt_start[0]=120; 
-  NEEt_start[1]=130; 
-  NEEt_start[2]=140; 
-  NEEt_start[3]=150; 
-  
-  NEPos_start[0]=126; 
-  NEPos_start[1]=136; 
-  NEPos_start[2]=146; 
+
+  HFEt_start[0]=8;
+  HFEt_start[1]=16;
+  HFEt_start[2]=24;
+  HFEt_start[3]=32;
+  HFEt_start[4]=40;
+  HFEt_start[5]=48;
+  HFEt_start[6]=56;
+  HFEt_start[7]=64;
+
+  HFFg_start[0]=72;
+  HFFg_start[1]=73;
+  HFFg_start[2]=74;
+  HFFg_start[3]=75;
+  HFFg_start[4]=76;
+  HFFg_start[5]=77;
+  HFFg_start[6]=78;
+  HFFg_start[7]=79;
+
+  IEEt_start[0]=80;
+  IEEt_start[1]=90;
+  IEEt_start[2]=100;
+  IEEt_start[3]=110;
+
+  IEPos_start[0]=86;
+  IEPos_start[1]=96;
+  IEPos_start[2]=106;
+  IEPos_start[3]=116;
+
+  NEEt_start[0]=120;
+  NEEt_start[1]=130;
+  NEEt_start[2]=140;
+  NEEt_start[3]=150;
+
+  NEPos_start[0]=126;
+  NEPos_start[1]=136;
+  NEPos_start[2]=146;
   NEPos_start[3]=156;
-  
-  RCHad_start[0][0]=160; 
-  RCHad_start[0][1]=161; 
-  RCHad_start[1][0]=162; 
-  RCHad_start[1][1]=163; 
-  RCHad_start[2][0]=164; 
-  RCHad_start[2][1]=165; 
-  RCHad_start[3][0]=166; 
-  RCHad_start[3][1]=167; 
-  RCHad_start[4][0]=168; 
-  RCHad_start[4][1]=169; 
-  RCHad_start[5][0]=170; 
-  RCHad_start[5][1]=171; 
-  RCHad_start[6][0]=172; 
-  RCHad_start[6][1]=173; 
+
+  RCHad_start[0][0]=160;
+  RCHad_start[0][1]=161;
+  RCHad_start[1][0]=162;
+  RCHad_start[1][1]=163;
+  RCHad_start[2][0]=164;
+  RCHad_start[2][1]=165;
+  RCHad_start[3][0]=166;
+  RCHad_start[3][1]=167;
+  RCHad_start[4][0]=168;
+  RCHad_start[4][1]=169;
+  RCHad_start[5][0]=170;
+  RCHad_start[5][1]=171;
+  RCHad_start[6][0]=172;
+  RCHad_start[6][1]=173;
 
 }
-
--- src/PhysicsToBitConverter.cc
+++ src/PhysicsToBitConverter.cc
@@ -1,4 +1,4 @@
-#include "PhysicsToBitConverter.h"
+#include "../interface/PhysicsToBitConverter.h"
 
 PhysicsToBitConverter::PhysicsToBitConverter() {
 
@@ -19,7 +19,7 @@ int PhysicsToBitConverter::GetRCEt(int card,int region){
   int mystart=databaseobject.GetIndicesRCEt(card,region);
   int mylength=databaseobject.GetRCEtLength();
   int mylink=databaseobject.GetRCEtLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -28,7 +28,7 @@ int PhysicsToBitConverter::GetHFEt(int region){
   int mystart=databaseobject.GetIndicesHFEt(region);
   int mylength=databaseobject.GetHFEtLength();
   int mylink=databaseobject.GetHFEtLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -37,7 +37,7 @@ int PhysicsToBitConverter::GetRCTau(int card,int region){
   int mystart=databaseobject.GetIndicesRCTau(card,region);
   int mylength=databaseobject.GetRCTauLength();
   int mylink=databaseobject.GetRCTauLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -46,7 +46,7 @@ int PhysicsToBitConverter::GetRCOf(int card,int region){
   int mystart=databaseobject.GetIndicesRCOf(card,region);
   int mylength=databaseobject.GetRCOfLength();
   int mylink=databaseobject.GetRCOfLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -55,7 +55,7 @@ int PhysicsToBitConverter::GetHFFg(int region){
   int mystart=databaseobject.GetIndicesHFFg(region);
   int mylength=databaseobject.GetHFFgLength();
   int mylink=databaseobject.GetHFFgLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -64,7 +64,7 @@ int PhysicsToBitConverter::GetNEPos(int cand){
   int mystart=databaseobject.GetIndicesNEPos(cand);
   int mylength=databaseobject.GetNEPosLength();
   int mylink=databaseobject.GetNEPosLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -74,7 +74,7 @@ int PhysicsToBitConverter::GetNEEt(int cand){
   std::cout<<"GetIndicesNEEt"<<mystart<<std::endl;
   int mylength=databaseobject.GetNEEtLength();
   int mylink=databaseobject.GetNEEtLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -83,7 +83,7 @@ int PhysicsToBitConverter::GetIEPos(int cand){
   int mystart=databaseobject.GetIndicesIEPos(cand);
   int mylength=databaseobject.GetIEPosLength();
   int mylink=databaseobject.GetIEPosLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -92,7 +92,7 @@ int PhysicsToBitConverter::GetIEEt(int cand){
   int mystart=databaseobject.GetIndicesIEEt(cand);
   int mylength=databaseobject.GetIEEtLength();
   int mylink=databaseobject.GetIEEtLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -101,7 +101,7 @@ int PhysicsToBitConverter::GetRCHad(int card,int region){
   int mystart=databaseobject.GetIndicesRCHad(card,region);
   int mylength=databaseobject.GetRCHadLength();
   int mylink=databaseobject.GetRCHadLink();
-  
+
   return BuildPhysicsValue(mystart,mylength,mylink);
 }
 
@@ -110,14 +110,14 @@ int PhysicsToBitConverter::ReadBitInInt(int bit,int value){
 
    std::bitset<32> foo(value);
    return foo[bit];
-  
+
 }
 
 int PhysicsToBitConverter::BuildPhysicsValue(int firstbit,int bitlength,int linkid){
-  
+
   int myvalue=0;
   int counter=0;
-  
+
   for (int m=firstbit;m<firstbit+bitlength;m++){
     myvalue|=(bitsLink[linkid][m]&(0x1)) << counter;
     counter++;
