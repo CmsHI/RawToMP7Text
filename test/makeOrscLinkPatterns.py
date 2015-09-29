b5139b6a1fd02b431d8468fe053e8befbc4f222f
R. Alex Barbieri <luck@mit.edu>
2015-09-29 19:43:49 +0200

    add 1000 new central HI events

--- test/makeOrscLinkPatterns.py
+++ test/makeOrscLinkPatterns.py
@@ -12,20 +12,60 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing('analysis')
 
 options.inputFiles = (
-    #'/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_100_1_cwZ.root'
-    #'file:/export/d00/scratch/luck/L1EmulatorTestInput.root'
-    #'file:/afs/cern.ch/work/g/ginnocen/public/SinglePi0Pt40-0000.root'
-    #'root://cmsxrootd-site.fnal.gov//store/user/lpctrig/apana/Collisions2015/pickevents_254790_HLTPhyspart0_IsoTau/Run254790_isolTau_1_1_Cai.root'
-    'root://cmsxrootd-site.fnal.gov//store/user/lpctrig/apana/Collisions2015/pickevents_254790_HLTPhyspart0_RlxTau/Run254790_rlxTau_1_1_q8a.root'
+    #'/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_166_1_NpX.root'
+    #"file:/afs/cern.ch/work/g/ginnocen/public/FilesForUnpackers/skim_10_1_wd2.root"
+
+# # 1
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_100_1_cwZ.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_101_1_Y8q.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_102_1_Tmr.root',
+
+#     #2
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_103_1_8aJ.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_104_1_aye.root',
+
+    ## 3
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_105_1_Muz.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_106_1_c2m.root',
+
+    ## 4
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_107_1_96s.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_108_1_xnb.root',
+
+    ## 5
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_109_1_NWe.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_10_1_0tY.root',
+
+    ## 6
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_110_1_fkm.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_112_1_sHP.root',
+
+    ## 7
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_114_1_K2E.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_115_1_h4h.root',
+
+    ## 8
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_120_1_RnI.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_121_1_2Fu.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_122_1_wlm.root',
+
+    ## 9
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_123_1_soE.root',
+# '/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_124_1_p5Y.root',
+
+    ## 10
+'/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_126_1_pCl.root',
+'/store/user/icali/HIMinBiasUPC/HIMinBiasUPC_Skim_HLT_HIMinBiasHfOrBSC_Centrality0-10//64ca16868e481177958780733023cfa2/SD_MB_Cen0_10_128_1_FuJ.root',
+
 )
-options.maxEvents = 3
+options.maxEvents = 100
 options.parseArguments()
 
 process = cms.Process("ORSCPATTERNS")
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_HLT_v1', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc_HIon', '')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(options.maxEvents)
@@ -37,16 +77,25 @@ process.source = cms.Source(
     )
 
 process.load("Configuration.Geometry.GeometryIdeal_cff")
-process.load("L1Trigger.L1TCalorimeter.L1TCaloStage1_PPFromRaw_cff")
-#process.load("L1Trigger.L1TCalorimeter.L1TCaloStage1_HIFromRaw_cff")
+#process.load("L1Trigger.L1TCalorimeter.L1TCaloStage1_PPFromRaw_cff")
+process.load("L1Trigger.L1TCalorimeter.L1TCaloStage1_HIFromRaw_cff")
+# process.load('L1Trigger.L1TCalorimeter.caloStage1RCTLuts_cff')
+# process.caloStage1Params.jetSeedThreshold = cms.double(0.)
+# process.RCTConfigProducers.eicIsolationThreshold = cms.uint32(7)
+# process.RCTConfigProducers.hOeCut = cms.double(999)
+# process.RCTConfigProducers.eMinForHoECut = cms.double(999)
+# process.RCTConfigProducers.eMaxForHoECut = cms.double(999)
+# process.RCTConfigProducers.hMinForHoECut = cms.double(999)
+# process.RCTConfigProducers.eMinForFGCut = cms.double(999)
+
 
 process.patterns = cms.EDAnalyzer('OrscLinkPatterns',
                                   src = cms.InputTag("simRctDigis"),
-                                  outputFile = cms.string("InputsoRSC/Run254790_rlxTau_withDelay.txt"))
+                                  outputFile = cms.string("InputsoRSC/HI_Central_Data_10.txt"))
 
 process.pattern_sequence = cms.Sequence(
-    process.L1TRerunHCALTP_FromRAW
-    #process.hcalDigis
+    #process.L1TRerunHCALTP_FromRAW
+    process.hcalDigis
     +process.ecalDigis
     +process.simRctDigis
     +process.patterns)
--- test/tests.cc
+++ test/tests.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../interface/DefineLinks.h"
 #include <math.h>
+#include "TString.h"
 
 
 int main(){
@@ -21,8 +22,18 @@ int main(){
     //linksdef->WriteFileMP7Format("OutputMP7/output_64eventsPion_WithDelay.txt");
 
     DefineLinks *linksdef=new DefineLinks();
-    linksdef->ReadFileoRSC("InputsoRSC/Run254790_rlxTau_withDelay.txt");
-    linksdef->WriteFileMP7Format("OutputMP7/Run254790_rlxTau_withDelay.txt");
+    for(int i = 1; i <= 10; i++)
+    {
+      std::string input = Form("InputsoRSC/HI_Central_Data_%.2d.txt",i);
+      std::string output = Form("OutputMP7/HI_Central_Data_%.2d.txt",i);
+      linksdef->ReadFileoRSC(input);
+      linksdef->WriteFileMP7Format(output);
+    }
+
+    // DefineLinks *linksdef2=new DefineLinks();
+    // linksdef2->ReadFileoRSC("InputsoRSC/pp100Events_HISettings.txt");
+    // linksdef2->WriteFileMP7Format("OutputMP7/pp100Events_HISettings.txt");
+
 
 //    DefineLinks *testreadMP7=new DefineLinks();
 //    testreadMP7->ReadFileMP7("OutputMP7/output_753539223.txt");
